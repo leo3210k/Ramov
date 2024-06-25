405de04ad73d0e2d71857471d902bff0ac43627f
leonardo <leocoelho.pi@gmail.com>
2024-06-25 20:45:25 -0300

    add contact h1

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,6 +4,10 @@ function Contact() {
   return (
     <div className="flex flex-col items-center gap-5 bg-contact py-14">
       <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full py-3 px-6">FALE COM A GENTE!</div>
+      <h1 className="text-3xl text-mine_shaft tracking-tight leading-10">
+        <span>Entre em contato </span> 
+        <b><i>conosco!</i></b>
+      </h1>
       
     </div>
   );
