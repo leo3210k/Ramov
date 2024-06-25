e8e61e5e6a9d0ad94979ffa80d62464591340a5e
leonardo <leocoelho.pi@gmail.com>
2024-06-25 19:52:58 -0300

    add hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,10 @@ function Hero() {
     <div className="relative h-screen flex items-center bg-hero bg-right bg-contain bg-no-repeat">
       <div className="w-2/6 flex flex-col text-left gap-6 ml-36">
         <img className="w-72" src="/assets/logo.svg" alt="" />
+        <h1 className="text-3xl tracking-tight leading-10">
+          <span>Garanta a Conformidade e Segurança dos seus Equipamentos com </span>
+          <b><i>Nossos Laudos Técnicos e ARTs!</i></b>
+        </h1>
         
       </div>
     </div>
