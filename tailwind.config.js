e251d5be2852bedf3f8f8847556529d6632f03f2
leonardo <leocoelho.pi@gmail.com>
2024-06-25 19:47:24 -0300

    add green darker and punga colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ module.exports = {
       colors: {
         sunglow: '#FBCA2E',
         green: '#00cf0a',
+        green_darker: '#10BE00',
         thunder: '#222122',
         baltic_sea: '#1B181C',
         mine_shaft: '#212121',
-        heavy_metal: '#1E1F1C'
+        heavy_metal: '#1E1F1C',
+        punga: '#473C16',
       },
       backgroundImage: {
         'hero': "url('../public/assets/matheus.svg')",
