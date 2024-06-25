d52c2d754ccda1be931b1cbc3dbc771e4154eed2
leonardo <leocoelho.pi@gmail.com>
2024-06-25 19:52:28 -0300

    add width to hero content and flex box

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 function Hero() {
   return (
     <div className="relative h-screen flex items-center bg-hero bg-right bg-contain bg-no-repeat">
-      
+      <div className="w-2/6 flex flex-col text-left gap-6 ml-36">
+        
+      </div>
     </div>
   );
 }
