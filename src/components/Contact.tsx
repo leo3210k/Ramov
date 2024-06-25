364720bb2bab7099b4640dcc74fa66fb66b7fd5e
leonardo <leocoelho.pi@gmail.com>
2024-06-25 20:44:55 -0300

    add contact title

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <div className="flex flex-col items-center gap-5 bg-contact py-14">
+      <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full py-3 px-6">FALE COM A GENTE!</div>
       
     </div>
   );
