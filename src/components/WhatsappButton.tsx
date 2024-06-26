a75b06a84f9873033506e5cf4639bba637609c59
leonardo <leocoelho.pi@gmail.com>
2024-06-26 15:42:47 -0300

    create whatsappButton component

---
+++ src/components/WhatsappButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function WhatsappButton() {
+  return (
+    <img className="fixed bottom-6 right-6" src="/assets/whatsapp_fixed.svg" alt="whatsapp" />
+  );
+}
+
+export default WhatsappButton;
