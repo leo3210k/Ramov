913d9142f5d35f3c611f835dd77d0314028276e0
leonardo <leocoelho.pi@gmail.com>
2024-06-27 19:59:42 -0300

    add services section title

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,6 +4,9 @@ import Service from './Service';
 function Services() {
   return (
     <div className="flex flex-col items-center gap-6 bg-service bg-cover bg-center py-16">
+      <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
+        contrate nossos serviços!
+      </div>
       
     </div>
   );
