80ba981f52433c5a8fed6a5fb760d76ce5f1dcdf
leonardo <leocoelho.pi@gmail.com>
2024-06-27 20:02:18 -0300

    add service card content

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -11,6 +11,9 @@ interface ServiceProps {
 function Service({ title, title_bold, background, position="center" }: ServiceProps) {
   return (
     <div className="w-52 h-[410px] flex flex-col items-center">
+      <div className={`service w-full h-[90%] flex flex-col justify-between items-center ${background} bg-${position} bg-cover`}>
+        
+      </div>
       
     </div>
   );
