ef039a5be13326b83647bc69be57c439b4efe4dc
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:03:58 -0300

    add about subtitle

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,9 @@ function About() {
   return (
     <div className="flex py-16">
       <div className="w-1/3 flex flex-col items-start gap-4 ml-44">
-        
+        <div className="text-xs text-mine_shaft font-semibold bg-sunglow rounded-full uppercase py-3 px-6">
+          sobre a ramov
+        </div>
         
         
         
