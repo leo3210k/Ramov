808066aed9d91f1605e094f5be0b65e09a62d174
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:07:05 -0300

    add footer copyright terms

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,9 @@ function Footer() {
     <div className="flex justify-center items-center border-t-[1px] border-mine_shaft py-10">
       <div className="flex flex-col items-center basis-1/2">
         <img className="w-36" src="/assets/icons/brand.svg" alt="brand" />
-        
+        <span className="text-sm text-punga text-center">
+          © 2024 KDP SOLUÇÕES EM ENGENHARIA – Todos os direitos reservados.
+        </span>
       </div>
       
     </div>
