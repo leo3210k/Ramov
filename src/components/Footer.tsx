73948742b87fdb872861b1b2d668ca50d0e76cd3
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:06:07 -0300

    add footer container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-   
+    <div className="flex justify-center items-center border-t-[1px] border-mine_shaft py-10">
+      
+      
+    </div>
   );
 }
 
