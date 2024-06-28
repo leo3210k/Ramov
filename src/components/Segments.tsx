16a7654f96f95d63df62b916f2dc1c7cec9bd755
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:00:50 -0300

    add segments title

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -8,7 +8,10 @@ function Services() {
       <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
         diversos segmentos!
       </div>
-      
+      <h1 className="text-3xl text-mine_shaft tracking-tight leading-10">
+        <span>Nossos serviços são </span> 
+        <b><i>ideais para:</i></b>
+      </h1>
       
       
     </div>
