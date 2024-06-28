f2576fe6a7cb8176e05688b06e3fdfd112849121
leonardo <leocoelho.pi@gmail.com>
2024-06-28 15:55:26 -0300

    add flex box to benefits

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Benefits() {
   return (
     <div className="h-[99vh] flex items-center bg-baltic_sea bg-benefits bg-[right_2rem] bg-contain bg-no-repeat">
-     
+      <div className="flex flex-col items-start gap-8 ml-48">
+        
+      </div>
     </div>
   );
 }
