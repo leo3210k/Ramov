50ff7e789528476aff317b5815095eb5d9758f67
leonardo <leocoelho.pi@gmail.com>
2024-06-28 15:56:06 -0300

    add benefits subtitle

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -4,6 +4,9 @@ function Benefits() {
   return (
     <div className="h-[99vh] flex items-center bg-baltic_sea bg-benefits bg-[right_2rem] bg-contain bg-no-repeat">
       <div className="flex flex-col items-start gap-8 ml-48">
+        <div className="text-xs text-mine_shaft font-semibold bg-sunglow rounded-full uppercase py-3 px-6">
+          aproveite as vantagens!
+        </div>
         
       </div>
     </div>
