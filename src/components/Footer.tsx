9b83291d78bfaef39919361ebe3d530a8dee6918
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:06:24 -0300

    add footer brand container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Footer() {
   return (
     <div className="flex justify-center items-center border-t-[1px] border-mine_shaft py-10">
-      
+      <div className="flex flex-col items-center basis-1/2">
+        
+        
+      </div>
       
     </div>
   );
