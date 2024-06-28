a49d3d0f5a12a7a544ac456084d93ca2da42a9ee
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:04:08 -0300

    add about title

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,10 @@ function About() {
         <div className="text-xs text-mine_shaft font-semibold bg-sunglow rounded-full uppercase py-3 px-6">
           sobre a ramov
         </div>
-        
+        <h1 className="text-3xl tracking-tight leading-10">
+          <span>Quem </span>
+          <b><i>Somos</i></b>
+        </h1>
         
         
         
