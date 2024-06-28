80b12a2a9cf3152bfb2c71d516e7172d2700dc76
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:03:38 -0300

    add flex box to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 function About() {
   return (
     <div className="flex py-16">
-      
+      <div className="w-1/3 flex flex-col items-start gap-4 ml-44">
+        
+        
+        
+        
+        
+      </div>
     </div>
   );
 }
