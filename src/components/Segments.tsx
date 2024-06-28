7ce7687c16f38043b5bf3af23939f53a397fde85
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:00:38 -0300

    add segments subtitle

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -5,7 +5,9 @@ import React from 'react';
 function Services() {
   return (
     <div className="flex flex-col items-center gap-6 py-16">
-      
+      <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
+        diversos segmentos!
+      </div>
       
       
       
