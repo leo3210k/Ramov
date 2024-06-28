a812156423aac3c595fa2cc6aac6dac8462686a6
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:09:23 -0300

    move benefits import

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,10 +1,13 @@
-import Benefits from '../components/Benefits';
 import Contact from '../components/Contact';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import WhatsappButton from '../components/WhatsappButton';
+import Benefits from '../components/Benefits';
+import Segments from '../components/Segments';
 import './App.css';
 import React from 'react';
+import About from '../components/About';
+import Footer from '../components/Footer';
 
 function App() {
   return (
