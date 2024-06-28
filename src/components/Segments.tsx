b9edd11d578006d2f6a64466da11fff73e29a0d1
leonardo <leocoelho.pi@gmail.com>
2024-06-28 16:00:15 -0300

    add segments container

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 
 function Services() {
   return (
-    
+    <div className="flex flex-col items-center gap-6 py-16">
+      
+      
+      
+      
+    </div>
   );
 }
 
