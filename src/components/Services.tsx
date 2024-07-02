ea255894ba78795c7f15b96229ce4c790ed47a3b
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:04:40 -0300

    change services background color to alabaster

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,7 +3,7 @@ import Service from './Service';
 
 function Services() {
   return (
-    <div className="flex flex-col items-center gap-6 bg-[#f1f1f1] bg-cover bg-center py-16">
+    <div className="flex flex-col items-center gap-6 bg-alabaster bg-cover bg-center py-16">
       <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
         contrate nossos serviços!
       </div>
