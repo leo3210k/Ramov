b23500985c6f56c859b4032c193a258038cfcd62
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:21:57 -0300

    add first information container on about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function About() {
   return (
     <div className="flex flex-col justify-center items-start gap-4 py-16 bg-about bg-center bg-cover bg-no-repeat p-10">
-      
+      <div className="w-1/3 flex flex-col gap-4 bg-mine_shaft_op rounded-sm p-6">
+        
+      </div>
       
     </div>
   );
