eed75862845eebee80c56bd891c0d1f1ef6a315a
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:10:07 -0300

    add location to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,12 @@ function Footer() {
         </span>
       </div>
       <div className="flex flex-col gap-4">
-        
+        <div className="flex items-center gap-4">
+          <div className="bg-red-700 rounded-full p-2">
+            <img className="w-5" src="/assets/icons/location.svg" alt="localidade" />
+          </div>
+          <span className="text-white font-medium">Rua Teófilo dos Santos, 1555, Morada do Sol</span>
+        </div>
         
         
       </div>
