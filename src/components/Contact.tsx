270f28d8097a98fd7132c3ff7b70619bf8a14682
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:17:34 -0300

    add background image to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import './Contact.css';
 
 function Contact() {
   return (
-    <div className="flex flex-col items-center gap-5 py-14">
+    <div className="flex flex-col items-center gap-5 bg-contact bg-cover bg-no-repeat bg-fixed py-14">
       <div className="text-xs text-white font-semibold border-solid border-[1px] border-white rounded-full py-3 px-6">FALE COM A GENTE!</div>
       <h1 className="text-3xl text-white tracking-tight leading-10">
         <span>Entre em contato </span> 
