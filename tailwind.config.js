69ab1648744b2e452e1784626f0f6460cb113bdc
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:28:03 -0300

    fix background image url

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,11 +23,11 @@ module.exports = {
         selective_yellow: '#FCBC04'
       },
       backgroundImage: {
-        'hero': "url('../public/assets/images/matheus1.svg')",
-        'contact': "url('../public/assets/images/contact-background1.jpg')",
+        'hero': "url('../public/assets/images/matheus.svg')",
+        'contact': "url('../public/assets/images/contact-background.jpg')",
         'service': "url('../public/assets/services/services_background.png')",
         'benefits': "url('../public/assets/images/benefits-background.png')",
-        'about': "url('../public/assets/images/about-background1.jpg')",
+        'about': "url('../public/assets/images/about-background.jpg')",
         'proj-climatizacao-5': "url('../public/assets/services/project/proj-climatizacao-5.webp')",
         'proj-exaustao-3': "url('../public/assets/services/project/proj-exaustao-3.jpg')",
         'proj-maqflux-4': "url('../public/assets/services/project/proj-maqflux-4.jpg')",
