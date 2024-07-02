d0990ddf332fe8c05dc02e75d7579d36cd044ec8
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:17:15 -0300

    change contact text color to white

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Contact.css';
 
 function Contact() {
   return (
-    <div className="flex flex-col items-center gap-5 bg-contact py-14">
-      <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full py-3 px-6">FALE COM A GENTE!</div>
-      <h1 className="text-3xl text-mine_shaft tracking-tight leading-10">
+    <div className="flex flex-col items-center gap-5 py-14">
+      <div className="text-xs text-white font-semibold border-solid border-[1px] border-white rounded-full py-3 px-6">FALE COM A GENTE!</div>
+      <h1 className="text-3xl text-white tracking-tight leading-10">
         <span>Entre em contato </span> 
         <b><i>conosco!</i></b>
       </h1>
-      <p className="w-3/6 text-center text-sm text-punga font-medium">
+      <p className="w-3/6 text-center text-sm font-medium">
         Entre em contato conosco para saber mais sobre 
         como nossos serviços podem beneficiar o seu negócio. 
         Estamos prontos para oferecer soluções personalizadas 
