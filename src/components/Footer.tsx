fe7c48c788f597bbd23db6c45402a08ab0be170c
leonardo <leocoelho.pi@gmail.com>
2024-07-02 20:08:38 -0300

    increase brand size on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="flex justify-evenly items-center bg-heavy_metal py-16">
       <div className="flex flex-col items-center">
+        <img className="w-48" src="/assets/icons/brand_light.svg" alt="brand" />
         <span className="text-sm text-white text-center font-medium">
           © 2024 KDP SOLUÇÕES EM ENGENHARIA – <br></br> Todos os direitos reservados.
         </span>
