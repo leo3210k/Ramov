b8603842faac10088c3f598532f1a34c88b08904
leonardo <leocoelho.pi@gmail.com>
2024-07-02 21:14:30 -0300

    create whatsapp button component

---
+++ src/components/utils/WhatsappButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WhatsappButton() {
+  return (
+    <div className="text-xs text-white font-semibold bg-red-700 rounded-full uppercase py-3 px-6">
+      aproveite as vantagens!
+    </div>
+  );
+}
+
+export default WhatsappButton;
