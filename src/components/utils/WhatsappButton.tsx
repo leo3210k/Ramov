ff21183aa9464fa7784d4f59d59b43465f62cffa
leonardo <leocoelho.pi@gmail.com>
2024-07-02 21:14:55 -0300

    add background color and text props

--- src/components/utils/WhatsappButton.tsx
+++ src/components/utils/WhatsappButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function WhatsappButton() {
+function WhatsappButton({ background_color, text}: { background_color: string, text: string }) {
   return (
     <div className="text-xs text-white font-semibold bg-red-700 rounded-full uppercase py-3 px-6">
       aproveite as vantagens!
