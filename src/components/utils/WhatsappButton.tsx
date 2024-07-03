a4d4f5de778062ae9a833c90718055b9fc71a1c4
leonardo <leocoelho.pi@gmail.com>
2024-07-02 21:16:00 -0300

    remove bg from background color

--- src/components/utils/WhatsappButton.tsx
+++ src/components/utils/WhatsappButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function WhatsappButton({ background_color, text, classes }: { background_color: string, text: string, classes?: string }) {
   return (
-    <a className={`flex gap-4 text-xs font-medium text-white bg-${background_color} rounded-sm uppercase py-5 px-10 mt-6 ${classes}`} 
+    <a className={`flex gap-4 text-xs font-medium text-white ${background_color} rounded-sm uppercase py-5 px-10 mt-6 ${classes}`} 
       href="">
       { text }
       <img src="/assets/icons/whatsapp.svg" alt="whatsapp" />
