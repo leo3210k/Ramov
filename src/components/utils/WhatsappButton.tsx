d99dc44a0e9f40a31055e903e49d84a260b4e8a2
leonardo <leocoelho.pi@gmail.com>
2024-07-03 16:33:43 -0300

    change color to background_color

--- src/components/utils/WhatsappButton.tsx
+++ src/components/utils/WhatsappButton.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as WhatsappIcon } from '../../assets/icons/whatsapp.svg'
 
 function WhatsappButton({ background_color, text, classes }: { background_color: string, text: string, classes?: string }) {
   return (
-    <a className={`flex gap-4 text-xs font-medium text-white ${background_color} border-[1px] group hover:border-royal_blue
+    <a className={`flex gap-4 text-xs font-medium text-white ${background_color} group hover:border-[1px] hover:border-royal_blue
       rounded-sm uppercase py-5 px-10 mt-6 hover:-translate-y-3 transition-transform ease-in-out duration-500	
       hover:bg-transparent hover:text-royal_blue ${classes}`} 
       href="">
