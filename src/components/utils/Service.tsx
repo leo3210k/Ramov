de35f54b4b9103632d6a954429d4ff4ce4231efd
leonardo <leocoelho.pi@gmail.com>
2024-07-03 20:32:28 -0300

    change height-10% to basis-10%

--- src/components/utils/Service.tsx
+++ src/components/utils/Service.tsx
@@ -16,9 +16,22 @@ function Service({ title, title_bold, background, position="center" }: ServicePr
   return (
     <div className={`w-52 ${knowMore ? 'h-auto' : 'h-[25.6rem]'} flex flex-col items-center`}>
       <div className={`service w-full basis-[90%] flex flex-col justify-between items-center ${background} bg-${position} bg-cover`}>
+        <div className="flex justify-center items-center bg-white px-1 py-1 shadow-3xl">
+          <img className="w-20" src={dark_brand} alt="brand" />
+        </div>
+        <div className="w-full flex flex-col items-center gap-12">
+          <span className="text-lg text-center text-white font-medium mx-4">
+            {title} <b><i>{title_bold}</i></b>
+          </span>
+          <div className="w-full flex flex-col py-1">
+            <hr className="border-[1.1px] border-gravel"/>
+            <span className="text-white text-[8px] font-medium self-center tracking-wider mt-1">Ramov Projetos e Soluções de Engenharia</span>
+          </div>
+        </div>
+      </div>
+      <div className={`w-full flex flex-col items-center ${knowMore ? 'basis-auto' : 'basis-[10%]'} transition-[basis] ease-in-out duration-500 bg-mine_shaft px-4 py-2`}>
         
       </div>
-      
     </div>
   );
 }
