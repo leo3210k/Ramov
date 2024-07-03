f8407f247e396e7e59b4a058e8b43396be5de7b6
leonardo <leocoelho.pi@gmail.com>
2024-07-02 21:15:46 -0300

    add classes prop

--- src/components/utils/WhatsappButton.tsx
+++ src/components/utils/WhatsappButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function WhatsappButton({ background_color, text }: { background_color: string, text: string }) {
+function WhatsappButton({ background_color, text, classes }: { background_color: string, text: string, classes?: string }) {
   return (
-    <a className={`flex gap-4 text-xs font-medium text-white bg-${background_color} rounded-sm uppercase py-5 px-10 mt-6`} 
+    <a className={`flex gap-4 text-xs font-medium text-white bg-${background_color} rounded-sm uppercase py-5 px-10 mt-6 ${classes}`} 
       href="">
       { text }
       <img src="/assets/icons/whatsapp.svg" alt="whatsapp" />
