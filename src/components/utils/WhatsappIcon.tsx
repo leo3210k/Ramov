2332c29feb31459f1477905842806e268165c686
leonardo <leocoelho.pi@gmail.com>
2024-07-02 21:18:34 -0300

    move files

--- src/components/utils/WhatsappIcon.tsx
+++ src/components/utils/WhatsappIcon.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function WhatsappButton() {
+function WhatsappIcon() {
   return (
     <img className="fixed bottom-6 right-6" src="/assets/icons/whatsapp_fixed.svg" alt="whatsapp" />
   );
 }
 
-export default WhatsappButton;
+export default WhatsappIcon;
