c4124a50d38cddc3bb1ea05d64d5ff6195c2569f
leonardo <leocoelho.pi@gmail.com>
2024-07-04 16:23:10 -0300

    fix padding vertical on segments

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -18,7 +18,7 @@ const Item_list = ({ number, text }: { number: string, text: string }) => {
 
 function Segments() {
   return (
-    <div className="flex flex-col items-center gap-6 py-16">
+    <div className="flex flex-col items-center gap-6 py-14">
       <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
         diversos segmentos!
       </div>
