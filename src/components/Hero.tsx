562da0b1dd5ed002b52cc7d7b1b2e38e87403af2
leonardo <leocoelho.pi@gmail.com>
2024-07-04 15:46:54 -0300

    fix hero height on desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import dark_brand from '../assets/icons/brand_dark.svg';
 function Hero() {
   return (
     <div className="relative h-[90vh] lg:h-screen flex items-center lg:bg-hero bg-right bg-contain bg-no-repeat p-8 lg:p-0">
-      <div className="h-full lg:w-2/6 flex flex-col justify-between lg:justify-normal text-left gap-6 lg:ml-36">
+      <div className="h-full lg:h-auto lg:w-2/6 flex flex-col justify-between lg:justify-normal text-left gap-6 lg:ml-36">
         <img className="w-36 md:w-40 lg:w-36" src={dark_brand} alt="brand" />
         <h1 className="text-3xl md:text-4xl lg:text-3xl tracking-tight leading-10">
           <span>Garanta a Conformidade e Segurança dos seus Equipamentos com </span>
