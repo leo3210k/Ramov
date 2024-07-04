842c400b302e32d0e7d8f43a40de015c66243cb5
leonardo <leocoelho.pi@gmail.com>
2024-07-04 19:51:57 -0300

    add text center to segments title

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -22,7 +22,7 @@ function Segments() {
       <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
         diversos segmentos!
       </div>
-      <h1 className="text-3xl text-mine_shaft tracking-tight leading-10">
+      <h1 className="text-3xl text-center text-mine_shaft tracking-tight leading-10">
         <span>Nossos serviços são </span> 
         <b><i>ideais para:</i></b>
       </h1>
