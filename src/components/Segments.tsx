8425ce435fe614a3fd98867b982fd8222d455f89
leonardo <leocoelho.pi@gmail.com>
2024-07-04 20:39:28 -0300

    add text center to segments bubble

--- src/components/Segments.tsx
+++ src/components/Segments.tsx
@@ -19,7 +19,7 @@ const Item_list = ({ number, text }: { number: string, text: string }) => {
 function Segments() {
   return (
     <div className="flex flex-col items-center gap-6 py-14">
-      <div className="text-xs text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
+      <div className="text-xs text-center text-mine_shaft font-semibold border-solid border-[1px] border-mine_shaft rounded-full uppercase py-3 px-6">
         diversos segmentos!
       </div>
       <h1 className="text-3xl text-center text-mine_shaft tracking-tight leading-10">
