66da80713d8595f3f15d2f769562fb5cd72b4883
leonardo <leocoelho.pi@gmail.com>
2024-07-04 20:31:41 -0300

    add items center to whatsappButton

--- src/components/utils/WhatsappButton.tsx
+++ src/components/utils/WhatsappButton.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as WhatsappIcon } from '../../assets/icons/whatsapp.svg'
 
 function WhatsappButton({ color, text, classes }: { color: string, text: string, classes?: string }) {
   return (
-    <a className={`flex gap-4 text-xs font-medium text-white 
+    <a className={`flex items-center gap-4 text-xs font-medium text-white 
     ${color === 'royal_blue' ? 'bg-royal_blue border-royal_blue' : 'bg-green border-green'} 
     border-[1px] group hover:border-royal_blue
     rounded-sm uppercase py-5 px-10 mt-6 hover:-translate-y-3 transition-transform ease-in-out duration-500	
