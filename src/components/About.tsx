5ff97c2e68d5691a4ee9f9519e97e55ef0555aee
leonardo <leocoelho.pi@gmail.com>
2024-07-09 16:29:48 -0300

    add text lg on about cards title on medium and below

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className="flex flex-col justify-center items-start gap-4 py-16 bg-about bg-center bg-cover bg-no-repeat p-10">
       <div className="w-1/3 flex flex-col gap-4 bg-mine_shaft_op rounded-sm p-6">
-        <span className="self-start text-xl text-white font-medium border-b-[1px] border-white tracking-tight leading-10 pb-2">
+        <span className="self-start text-lg md:text-xl text-white font-medium border-b-[1px] border-white tracking-tight leading-10 pb-2">
           Quem Somos?
         </span>
         <span className="text-sm text-white leading-5">
