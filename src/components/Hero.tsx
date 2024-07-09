c5301e4de5ca27783dd2318052195ab2c78f1a88
leonardo <leocoelho.pi@gmail.com>
2024-07-09 16:13:07 -0300

    change height to only on xs and ahead

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import dark_brand from '../assets/icons/brand_dark.svg';
 
 function Hero() {
   return (
-    <div className="relative h-[90vh] lg:h-screen flex items-center lg:bg-hero bg-right bg-contain bg-no-repeat p-8 lg:p-0">
+    <div className="relative xs:h-[90vh] lg:h-screen flex items-center lg:bg-hero bg-right bg-contain bg-no-repeat p-8 lg:p-0">
       <div className="h-full lg:h-auto lg:w-2/6 flex flex-col justify-between lg:justify-normal text-left gap-6 lg:ml-36">
         <img className="w-36 md:w-40 lg:w-36" src={dark_brand} alt="brand" />
         <h1 className="text-3xl md:text-4xl lg:text-3xl tracking-tight leading-10">
