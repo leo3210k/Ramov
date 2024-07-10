a5937af6b209dcb1a2f1d8f08f4c136584dc8491
leonardo <leocoelho.pi@gmail.com>
2024-07-10 16:44:35 -0300

    change width on xs and below

--- src/components/utils/WhatsappFixed.tsx
+++ src/components/utils/WhatsappFixed.tsx
@@ -3,7 +3,7 @@ import whatsappIcon from '../../assets/icons/whatsapp_fixed.svg'
 
 function WhatsappIcon() {
   return (
-    <img className="fixed bottom-6 right-6" src={whatsappIcon} alt="whatsapp" />
+    <img className="w-12 xs:w-auto fixed bottom-6 right-6" src={whatsappIcon} alt="whatsapp" />
   );
 }
 
