b296cd41514f83a2053beef1d18b583fb54a2559
leonardo <leocoelho.pi@gmail.com>
2024-07-11 12:08:13 -0300

    add justify between to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import profile from '../assets/images/matheus.svg';
 
 function Hero() {
   return (
-    <div className="relative lg:h-screen flex items-center p-8 lg:p-0">
+    <div className="relative lg:h-screen flex justify-between items-center p-8 lg:p-0">
       <div className="h-full lg:h-auto lg:w-2/6 flex flex-col justify-between lg:justify-normal text-left gap-6 lg:ml-36">
         <img className="w-36 md:w-40 lg:w-36" src={dark_brand} alt="brand" />
         <h1 className="text-3xl md:text-4xl lg:text-3xl tracking-tight leading-10">
