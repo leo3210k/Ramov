954942075c0f6b743fab440afe5b8545417e5ec2
leonardo <leocoelho.pi@gmail.com>
2024-07-11 17:12:35 -0300

    change item list component name

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import WhatsappButton from './utils/WhatsappButton';
 import right_arrow from '../assets/icons/right_arrow.svg'
 
-const Item_list = ({ text }: { text: string }) => {
+const ItemList = ({ text }: { text: string }) => {
   return (
     <div className="flex items-center gap-3">
       <div className="min-w-fit bg-red-700 rounded-full p-1">
@@ -25,12 +25,12 @@ function Benefits() {
           <b className="text-red-700"><i>Exclusivos</i></b>
         </h1>
         <div className="flex flex-col gap-4">
-          <Item_list text="Conformidade com normas e regulamentos;" />
-          <Item_list text="Segurança e proteção legal;" />
-          <Item_list text="Valorização e segurança dos equipamentos;"/>
-          <Item_list text="Qualidade do ar e segurança dos ambientes;" />
-          <Item_list text="Preservação da integridade dos veículos;" />
-          <Item_list text="Acompanhamento técnico especializado." />
+          <ItemList text="Conformidade com normas e regulamentos;" />
+          <ItemList text="Segurança e proteção legal;" />
+          <ItemList text="Valorização e segurança dos equipamentos;"/>
+          <ItemList text="Qualidade do ar e segurança dos ambientes;" />
+          <ItemList text="Preservação da integridade dos veículos;" />
+          <ItemList text="Acompanhamento técnico especializado." />
         </div>
         <WhatsappButton text="aproveitar benefícios" color="royal_blue"/>
       </div>
