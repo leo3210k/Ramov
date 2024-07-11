8b7a1bcc9811707d16a56c4c8115c4f2101e3082
leonardo <leocoelho.pi@gmail.com>
2024-07-11 12:08:30 -0300

    change width for height on hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ function Hero() {
         </span>
         <WhatsappButton text="Solicitar orçamento!" color="green" classes="self-center lg:self-start" />
       </div>
-      <img className="w-1/2" src={profile} alt="perfil" />
+      <img className="h-full" src={profile} alt="perfil" />
     </div>
   );
 }
