8039d11ae95c9f826559420807419cd8c6350c89
leonardo <leocoelho.pi@gmail.com>
2024-07-15 19:44:28 -0300

    remove hero description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,13 +12,13 @@ function Hero() {
           <span>Garanta a Qualidade e Conformidade dos seus Projetos com a </span>
           <b><i>RamoV Engenharia!</i></b>
         </h1>
-        <span className="text-sm md:text-base lg:text-sm font-medium	text-punga">
+        {/* <span className="text-sm md:text-base lg:text-sm font-medium	text-punga">
           Bem-vindo à RamoV Engenharia! Oferecemos soluções completas e integradas em 
           engenharia mecânica, focando em inovação, segurança e eficiência. Nossos serviços 
           incluem projetos de ar-condicionado, refrigeração, sistemas mecânicos e mais. Com uma 
           equipe experiente e capacitada, garantimos excelência, compromisso e sustentabilidade. 
           Transforme suas ideias em realidade com a RamoV Engenharia!
-        </span>
+        </span> */}
         <WhatsappButton text="Solicitar orçamento!" color="green" classes="self-center lg:self-start" />
       </div>
       <img className="hidden lg:block h-full self-end" src={profile} alt="perfil" />
