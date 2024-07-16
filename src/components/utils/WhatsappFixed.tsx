ac6fb5ddc5cf21ee50fdedddcb857647b2ef4f71
leonardo <leocoelho.pi@gmail.com>
2024-07-16 11:26:23 -0300

    add hover scale on whatsapp fixed

--- src/components/utils/WhatsappFixed.tsx
+++ src/components/utils/WhatsappFixed.tsx
@@ -4,7 +4,7 @@ import whatsappIcon from '../../assets/icons/whatsapp_fixed.svg'
 function WhatsappIcon() {
   return (
   <a href="http://wa.me/5586988858856" target="_blank">
-    <img className="w-12 xs:w-[4.5rem] fixed bottom-6 right-6" src={whatsappIcon} alt="whatsapp" />
+    <img className="w-12 xs:w-[4.5rem] fixed bottom-6 right-6 hover:scale-110 transition-transform ease-in-out" src={whatsappIcon} alt="whatsapp" />
   </a>
   );
 }
