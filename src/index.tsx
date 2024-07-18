99d443993c4a05711f5cb39870d688feb1fd7384
leonardo <leocoelho.pi@gmail.com>
2024-07-18 12:16:23 -0300

    change analytics tag from file

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './main/App';
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics/>
     <SpeedInsights/>
   </React.StrictMode>
 );
