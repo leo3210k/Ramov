2a71512215dd1615387d8ec69849029ac300f55a
leonardo <leocoelho.pi@gmail.com>
2024-07-18 12:12:42 -0300

    add speed insights to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './main/App';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>
 );
